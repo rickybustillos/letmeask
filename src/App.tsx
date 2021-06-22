c296c0bc49fcaebe776de5bdfa3f0721fc8e9262
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-06-22 17:03:20 -0300

    feat: created Home page
    Created Home page, configured global styles and created the Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import { Button } from './components/Button';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <Home />
   );
 }
 
