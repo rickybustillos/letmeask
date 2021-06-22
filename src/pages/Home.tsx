998ec0a095dd8004187a304cbabea8d12968b4e3
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-06-22 18:28:31 -0300

    feat: created Authentication system
    Create full Authentication system (contexts, hooks and firebase auth)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,24 @@
 import { useHistory } from 'react-router';
 
+import { useAuth } from '../hooks/useAuth';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
-import '../styles/auth.scss';
 import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
 
-  function navigateToNewRoom() {
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
     history.push('/rooms/new');
   }
 
@@ -24,13 +32,13 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={navigateToNewRoom} className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
           <form>
-            <input type="text" placeholder="Digite o código da sala"/>
+            <input type="text" placeholder="Digite o código da sala" />
             <Button type="submit">
               Entrar na sala
             </Button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,13 +1,17 @@
+import { useAuth } from '../hooks/useAuth';
 import { Link } from 'react-router-dom';
+
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 
-import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
+import '../styles/auth.scss';
+
 export function NewRoom() {
+  const { user } = useAuth();
+  
   return (
     <div id="page-auth">
       <aside>
