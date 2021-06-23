d503fcd9565ffcb2038038fa0893f41985118836
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-06-23 14:59:37 -0300

    feat: Authenticated user data in Room page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -69,7 +69,14 @@ export function Room() {
           />
 
           <div className="form-footer">
-            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            { user ? (
+                <div className="user-info">
+                  <img src={user.avatar} alt={user.name} />
+                  <span>{user.name}</span>
+                </div>
+              ) : (
+              <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+              ) }
             <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
