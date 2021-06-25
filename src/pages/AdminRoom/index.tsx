e37d1432a72b08f30f36eebc2e9da08d28594378
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-06-25 01:49:46 -0300

    feat: updated Question buttons
    Updated Question buttons for AdminRoom and Room pages:
    - Like (Room)
    - Highlight (Admin)
    - Answered (Admin)
    - Delete (Admin)
    Updated Question styles for this new features.
    - Added lib classnames

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -5,6 +5,8 @@ import { useHistory, useParams } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import deleteImg from '../../assets/images/delete.svg';
+import checkImg from '../../assets/images/check.svg';
+import answerImg from '../../assets/images/answer.svg';
 
 import { database } from '../../services/firebase';
 
@@ -13,6 +15,7 @@ import { RoomCode } from '../../components/RoomCode';
 import { Question } from '../../components/Question';
 
 import { useRoom } from '../../hooks/useRoom';
+import { Fragment } from 'react';
 
 type RoomParams = {
   id: string;
@@ -39,14 +42,27 @@ export function AdminRoom() {
     }
   }
 
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true
+    });
+
+  }
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
           <div>
-          <RoomCode code={roomId} />
-          <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
+            <RoomCode code={roomId} />
+            <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
           </div>
         </div>
       </header>
@@ -64,7 +80,25 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
+                isAnswered={question.isAnswered}
+                isHighlighted={question.isHighlighted}
               >
+                { !question.isAnswered && (
+                  <Fragment>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Marcar pergunta como respondida" />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque à pergunta" />
+                    </button>
+                  </Fragment>
+                )}
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
